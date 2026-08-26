97e5b38a6e6e4f2a702d17b13cdfa263bd4a917c
Shpoodr <heyheylogan.m@gmail.com>
2026-08-25 17:23:54 -0700

    fixed the dumb ahh bug clicking sends a message now

--- Source/Medivail/Core/MedivailPlayerController.cpp
+++ Source/Medivail/Core/MedivailPlayerController.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "MedivailPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
-#include "MedivailPlayerController.h"
 
 void AMedivailPlayerController::BeginPlay() {
 	Super::BeginPlay();
